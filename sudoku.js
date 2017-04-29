5ace8b6f5189f76ae4f19efe0f2a38cd4e54aa61
raynormw <tirtawiryaputra@yahoo.com>
2017-04-29 22:05:33 +0700

    remove unused variable

--- sudoku.js
+++ sudoku.js
@@ -21,7 +21,6 @@ class Sudoku {
 
   solve() {
     let solvedArray = [];
-    let number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
     for (let i = 0; i < this.unsolved.length; i++) {
       let temp = this.recursive(this.unsolved[i]);
