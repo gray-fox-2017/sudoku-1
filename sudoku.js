6c52b3d92caad3ddbdfb61eaf2662279ac299889
Zulfikar Annur Ahmad <zlfikar.aa@gmail.com>
2017-04-30 14:28:46 +0700

    disolve malah berantakan

--- sudoku.js
+++ sudoku.js
@@ -24,17 +24,33 @@ class Sudoku {
   solve() {
     this.solved = this.big_board;
     //Check and Fill Horizontally Randomly
-    for (let m = 0; m < this.solved.length; m++){
-      for (let n = 0; n < this.solved[m].length; n++){
-        // console.log(solving[m][n]);
-        if (this.solved[m][n] === 0){
-          this.solved[m][n] = Math.ceil(Math.random()*9);
+    // for (let m = 0; m < this.solved.length; m++){
+    //   for (let n = 0; n < this.solved[m].length; n++){
+    //     // console.log(solving[m][n]);
+    //     if (this.solved[m][n] === 0){
+    //       this.solved[m][n] = Math.ceil(Math.random()*9);
+    //     }
+    //   }
+    // }
+    //Check Horizontally
+    for (let h = 0; h < this.solved.length; h++){
+      for(let j = 0; j < this.solved[h].length; j++){
+        let tmp_2 = this.solved[h][j];
+        for (let p = 0; p < this.solved[h].length - 1; p++){
+          if (tmp_2 === this.solved[h][p + 1]){
+            this.solved[h][p + 1] = Math.ceil(Math.random()*9);
+            debugger;
+          }
+          debugger;
         }
+        debugger;
       }
+      debugger;
     }
+    debugger;
     //Check and Fill Vertically
     //Check and Fill by Small-Board
-    // console.log(this.solved);
+    console.log(this.solved);
   }
 
   // Returns a string representing the current state of the board
