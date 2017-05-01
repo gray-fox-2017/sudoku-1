7ff0978de78f58580c96a340454e9be0e33f2809
sidikriders <your@example.com>
2017-05-02 01:58:34 +0700

    pushyeng

--- sudoku.js
+++ sudoku.js
@@ -5,35 +5,60 @@ class Sudoku {
     this.str = str.split('');
     this.board = [];
     this.line = this.str.map( x => x + ' ');
+    this.zero = []
+    this.baris = []
+    this.kolom = []
+    this.kotak = []
   }
 
-  solve() {
-
-    var perbaris = []
-
-    for (let i=0; i<this.line.length; i=+9) {
-      perbaris.push(this.line.slice(i,i+9));
-    }
-
-    var perkolom = [];
-
-    for (let i=0; i<9; i++) {
-      perkolom.push(this.line[i]);
-    }
-
-
-
-    function definite(line) {
-      for (i=0;i<line.length;i++) {
-        if (i === '0') {
-          for (i=1;i<=9;i++) {
-
-          }
-        }
+  daftar_zero() {
+    for (let i=0;i<this.str.length;i++) {
+      if (this.str[i] === '0') {
+        this.zero.push(i);
       }
     }
-    return perkolom;
+
+    return this.zero
   }
+
+  group_baris() {
+    for (let i=0; i<this.str.length; i+=9) {
+      this.baris.push(this.str.slice(i,i+9));
+    }
+
+    return this.baris
+  }
+
+  group_kolom() {
+
+    var kolomSementara = []
+
+    for (let i=0; i<9;i++) {
+      for (let a=0; a<this.str.length; a+=9) {
+        kolomSementara.push(this.str[a+i]);
+      }
+    }
+
+    for (let i=0; i<kolomSementara.length; i+=9) {
+      this.kolom.push(kolomSementara.slice(i, i+9))
+    }
+    return this.kolom;
+  }
+
+  group_kotak() {
+
+    var kotakSementara = [];
+
+
+    for (let i=0;i<this.str.length;i+=3) {
+      kotakSementara.push((this.str.slice(i,i+3)).join(''))
+    }
+
+    return kotakSementara;
+
+  }
+
+  solve() {}
 
   // Returns a string representing the current state of the board
   print_board() {
@@ -67,4 +92,4 @@ class Sudoku {
 var game = new Sudoku('105802000090076405200400819019007306762083090000061050007600030430020501600308900');
 
 // Remember: this will just fill out what it can and not "guess"
-console.log(game.print_board());
+console.log(game.group_kotak());
